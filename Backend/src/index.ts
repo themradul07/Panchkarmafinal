463f53356c2b874fd8b537d9e17025357b7b8f5c
Anuj910577 <anujtiwari910577@gmail.com>
2025-09-28 12:46:41 +0530

    Update index.ts

--- Backend/src/index.ts
+++ Backend/src/index.ts
@@ -10,7 +10,7 @@ import authRouter from './routes/auth';
 dotenv.config();
 
 const app = express();
-const PORT = process.env.PORT || 5000;
+const mongoUri = process.env.MONGO_URI || "mongodb://localhost:27017/mydb";
 
 app.use(cors());
 app.use(express.json());
